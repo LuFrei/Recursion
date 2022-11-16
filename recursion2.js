1d844a65885ca34590a43392f2f2157a95d6051d
LuFrei <lucasfreitas09876@gmail.com>
2022-11-16 12:15:47 -0500

    exercise setup

---
+++ recursion2.js
@@ -0,0 +1,4 @@
+// Write a function, countVowels, that accepts a string as an argument and returns the number of vowels in that string.
+// Use recursion.
+
+countVowels('Four score and seven years'); // => 9
